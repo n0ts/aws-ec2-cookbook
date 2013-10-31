1ff7cf316845dace911c546880441b8ad533985f
n0ts <me@n0ts.org>
2013-10-31 17:44:33 +0900

    Added metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures aws_ec2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w{ apt python }.each do |depend|
+%w{ apt common python }.each do |depend|
   depends depend
 end
