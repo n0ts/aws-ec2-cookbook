31b1562fef20b308972db124e4c9d068f5074e31
n0ts <me@n0ts.org>
2013-10-31 20:35:53 +0900

    Added attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Credentials for cli
 # User Name,Access Key Id,Secret Access Key
 default[:aws_ec2][:awscli_credentials_csv] = ""
 
+default[:aws_ec2][:emrcli] = {
+  :data_bag_load_secret_path => "",
+  :data_bag_load_path => "",
+  :data_bag_path => "",
+  :data_bag_name => "",
+  :keypair_name => "",
+  :key_pair_file => "",
+  :log_uri => "",
+  :regiion => "",
+  :user => "root",
+}
